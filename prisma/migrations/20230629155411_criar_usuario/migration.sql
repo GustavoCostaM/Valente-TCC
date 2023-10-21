30f62f95e1d1da338b294b56d4910d55cde38db1
Gustavo Costa <gustavo.costa.moraes@gmail.com>
2023-10-21 02:34:04 -0300

    Perfil

--- prisma/migrations/20230629155411_criar_usuario/migration.sql
+++ prisma/migrations/20230629155411_criar_usuario/migration.sql
@@ -1,12 +1,12 @@
 -- CreateTable
 CREATE TABLE `Usuario` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `id` VARCHAR(191) NOT NULL,
     `nome` VARCHAR(255) NOT NULL,
     `email` VARCHAR(160) NOT NULL,
     `telefone` CHAR(11) NOT NULL,
     `senha` CHAR(60) NOT NULL,
-    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3);
-    `customer_id` VARCHAR(191) NULL;
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `customer_id` VARCHAR(191) NULL,
 
 
     UNIQUE INDEX `Usuario_email_key`(`email`),
